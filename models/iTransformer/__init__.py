346cdc3205fad3bc541ec150bfbac7d19bcf003a
laowu-code <70126706+laowu-code@users.noreply.github.com>
2025-06-30 14:38:48 +0800

    Delete models directory

--- models/iTransformer/__init__.py
+++
@@ -1,4 +0,0 @@
-# from iTransformer.iTransformer2D import iTransformer2D
-#
-# from iTransformer.iTransformerFFT import iTransformerFFT
-from models.iTransformer.iTransformer import iTransformer_block,iTransformer_single
